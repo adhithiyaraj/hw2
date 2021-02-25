7ac914ee51fd42340a4d1afe1324b74178ee279a
EC2 Default User <ec2-user@ip-172-31-23-71.us-east-2.compute.internal>
2021-02-25 16:27:41 +0000

    part 3 done

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,7 +1,3 @@
 class Movie < ActiveRecord::Base
-    def self.all_ratings
-        ratings = Array.new
-        self.select("rating").uniq.each {|x| ratings.push(x.rating)}
-        ratings.sort.uniq
-    end
+    
 end
